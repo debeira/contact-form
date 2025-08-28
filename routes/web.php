9583e3f7f4a2765e97ade1471d3aab08fdb02c15
Arno Debeir <arnodebeir@Big-Mac.local>
2025-08-28 15:10:36 +0200

    Added login routes and protect the admin routes

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\MessageController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', [ContactController::class, 'index'])->name('contact.index');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
 
-Route::get('/inzendingen', [MessageController::class, 'index'])->name('messages.index');
-Route::patch('/messages/{message}/read', [MessageController::class, 'markRead'])->name('messages.read');
+Route::middleware('auth')->group(function () {
+    Route::get('/inzendingen', [MessageController::class, 'index'])->name('messages.index');
+    Route::patch('/messages/{message}/read', [MessageController::class, 'markRead'])->name('messages.read');
+});
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.perform');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
